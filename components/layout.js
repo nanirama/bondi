ba10df8ec04ce2bf44f8497908f4308a9b82e879
nanirama <nani.rama1982@gmail.com>
2022-07-18 18:47:03 +0530

    c1

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import Header from "./header"
+import Footer from "./footer"
+// import Seo from "./seo"
+
+
+
+const Layout = ({ children, seo }) => (
+  <>
+    <Header />
+    {children}
+    <Footer/>
+  </>
+)
+
+export default Layout
