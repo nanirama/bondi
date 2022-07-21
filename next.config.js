8b329eb4b09046f3cde5f256ad29062aa0be7a7a
nanirama <nani.rama1982@gmail.com>
2022-07-21 21:06:17 +0530

    f

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? 'https://sites.trady.com' : '',
   images: {
     loader: "imgix",
-    path: 'https://sites.trady.com/',
+    path: isProd ? '/' : process.env.NEXT_PUBLIC_FRONTEND_URL,
   }
 }
 
